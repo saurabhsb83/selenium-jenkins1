214f677bbf4b678a2868da79c333d218d96ebea1
Saurabh <saurabh.sb83@gmail.com>
2020-05-20 15:30:48 +0530

    Application commited to github - version 1

---
+++ src/main/java/com/springdemo/Selenium1/Selenium1Application.java
@@ -0,0 +1,57 @@
+package com.springdemo.Selenium1;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Selenium1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Selenium1Application.class, args);
+		
+		
+		System.setProperty("webdriver.chrome.driver", "D:\\chromedriver_win32\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		
+		driver.manage().timeouts().pageLoadTimeout(30, TimeUnit.SECONDS);
+		
+		driver.get("https://www.apache.org/");
+		
+		System.out.println("Navigated to Apache !!");
+		
+		try {
+			Thread.sleep(4000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		driver.findElement(By.xpath("//a[contains(text(),'About')]")).click();
+		
+		try {
+			Thread.sleep(3000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		driver.findElement(By.xpath("//a[contains(text(),'Process')]")).click();
+		
+		try {
+			Thread.sleep(3000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+		System.out.println("Closing browser and destroying driver object !");
+		driver.close();
+	}
+
+}
